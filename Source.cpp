03cfedfe251bab8ac23c44c1d4800490024bed9e
mahan <mahan.mohammadi.a@gmail.com>
2026-08-11 20:45:40 +0330

    defined the loop function

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 void loop() {
+	int status;
+	char* line;
+	char** args;
 
+	do {
+		printf("> ");
+
+		line = read_line();
+		args = parse(line);
+		status = execute(args);
+	} while (status);
 }
 int main() {
 	loop();
