e226d238d3fa47fe444668228119dee787c9d324
mahan <mahan.mohammadi.a@gmail.com>
2026-08-13 22:47:15 +0330

    added parsing

--- Source.c
+++ Source.c
@@ -1,4 +1,36 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <string.h>
+#define TOKEN 64
+#define DELI " \t\a\r\n"
+
+char* parse(char * line){
+	int position = 0;
+	int buffersize = TOKEN;
+	char** tokens = malloc(buffersize * sizeof(char*));
+	char* token;
+
+	token = strtok(line, DELI);
+	while (token) {
+		tokens[position] = token;
+		position++;
+		if (position >= buffersize) {
+			buffersize += TOKEN;
+			tokens = realloc(tokens, buffersize * sizeof(char*));
+
+		}
+
+		token = strtok(NULL, DELI);
+
+	}
+	tokens[position] = NULL;
+	return tokens;
+
+}
+
+
 char* read_line() {
 	int buffersize = 64;
 	int position = 0;
@@ -32,13 +64,15 @@ void loop() {
 	char* line;
 	char** args;
 
+	status = 1;
+
 	do {
 		printf("> ");
 
 		line = read_line();
 		args = parse(line);
 		status = execute(args);
-
+		printf(line);
 		free(line);
 		free(args);
 	} while (status);
